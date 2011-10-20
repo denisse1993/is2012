534a29f0f230c0d2bcebefd370d81ed156407b1a
joseangel.jstyl8 <joseangel.jstyl8@gmail.com>
2011-10-20 14:48:34 +0000

    Add Borrador Proyecto Edu

---
+++ Borrador/src/com/Cinnamon/Is/Intro.java
@@ -0,0 +1,46 @@
+package com.Cinnamon.Is;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+
+public class Intro extends Activity {
+	
+	private MediaPlayer introTheme;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro);
+        introTheme =  MediaPlayer.create(Intro.this, R.raw.hearthbeat_sound);
+        introTheme.start();
+        Thread timer = new Thread(){
+			public void run() {
+				// TODO Auto-generated method stub
+				try{
+					sleep(3650);
+				}
+				catch(InterruptedException e){
+					e.printStackTrace();
+				}
+				finally {
+					Intent openMainMenu = new Intent("com.Cinnamon.Is.MAINMENU");
+					startActivity(openMainMenu);
+				}
+			}        	
+        };
+        timer.start();        
+    }
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		introTheme.release();
+		finish();
+	}
+    
+    
+}
