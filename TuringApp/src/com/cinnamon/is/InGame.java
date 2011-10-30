7ed3b1b8b66df5d958ff2f04fe5f3be958cc2754
joseangel.jstyl8 <joseangel.jstyl8@gmail.com>
2011-10-30 17:00:06 +0000

    Aplicación TuringApp, con lectorQR implementado. Intents - Clase de utilidad para agrupar los Actions Minijuego,AscensorMJ - Estructura Minijuegos básica añadida InGame - Estructura pantalla de juego básica añadida

---
+++ TuringApp/src/com/cinnamon/is/InGame.java
@@ -0,0 +1,109 @@
+package com.cinnamon.is;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+/**
+ * Actividad del juego principal
+ * 
+ * @author Cinnamon Team
+ * 
+ */
+public class InGame extends Activity implements OnClickListener {
+
+	// base de datos?¿
+
+	private Button bMapa, bCamara, bMochila;
+
+	private String qrLeido;
+
+	private String[] minijuegos = { "AscensorMJ" };
+
+	// Constantes para controlar que actividad tratar en onActivityResult
+	private static final int cMapa = 0;
+	private static final int cCamara = 1;
+	private static final int cMochila = 2;
+	private static final int cMinijuego = 3;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ingame);
+		inicializar();
+	}
+
+	private void inicializar() {
+		bMapa = (Button) findViewById(R.id.bMapa);
+		bCamara = (Button) findViewById(R.id.bCamara);
+		bMochila = (Button) findViewById(R.id.bMochila);
+
+		bMapa.setOnClickListener(this);
+		bCamara.setOnClickListener(this);
+		bMochila.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.bMapa:
+			break;
+		case R.id.bCamara:
+			Intent iScan = new Intent(Intents.Action.SCAN);
+			iScan.putExtra("SCAN_MODE", "QR_CODE_MODE");
+			startActivityForResult(iScan, cCamara);
+			break;
+		case R.id.bMochila:
+			break;
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		
+		if (resultCode == RESULT_OK) {
+			switch (requestCode) {
+			case cMapa:
+				break;
+			case cCamara:
+				qrLeido = data.getStringExtra("SCAN_RESULT");
+				lanzaMinijuego(qrLeido);
+				break;
+			case cMochila:
+				break;
+			case cMinijuego:
+				break;
+			}
+
+		} else if (resultCode == RESULT_CANCELED) {
+			// si no ha hecho nada
+		}
+	}
+
+	/**
+	 * Método que lanza el minijuego del "qrMinijuego", comprobando que el
+	 * codigoQR leido es correcto para lanzar la actividad asociada
+	 * 
+	 * @param qrMiniJuego
+	 *            el nombre del minijuego a lanzar
+	 */
+	private void lanzaMinijuego(String qrMiniJuego) {
+
+		for (int i = 0; i < minijuegos.length; i++) {
+			// comprobacion de que existe el minijuego leido
+			if (minijuegos[i].equals(qrMiniJuego)) {
+				Intent iMinijuego = new Intent(Intents.Comun.BASE + qrMiniJuego);
+				startActivityForResult(iMinijuego, cMinijuego);
+			}
+		}
+		// no se lanza el minijuego porque no se corresponde con uno que exista
+	}
+
+}
---
+++ TuringApp/src/com/cinnamon/is/Intro.java
@@ -0,0 +1,43 @@
+package com.cinnamon.is;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+public class Intro extends Activity {
+
+	private MediaPlayer introTheme;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.intro);
+		introTheme = MediaPlayer.create(Intro.this, R.raw.hearthbeat_sound);
+		introTheme.start();
+		Thread timer = new Thread() {
+			public void run() {
+				// TODO Auto-generated method stub
+				try {
+					sleep(3650);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					Intent openIntro2 = new Intent(Intents.Action.INTRO2);
+					startActivity(openIntro2);
+				}
+			}
+		};
+		timer.start();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		introTheme.release();
+		finish();
+	}
+
+}
