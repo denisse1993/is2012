e6f73a60a014ed1c84f671295d03a5d67457e987
edu.t09@gmail.com <edu.t09@gmail.com@2ab73cf0-c2b9-5e7a-3472-acf655e2b59c>
2011-11-14 11:40:08 +0000

    He cambiado el main menu y le he puesto una imagen de fondo y he cambiado también el botón Nueva Partida. He creado una clase en un nuevo paquete custom button que ahora mismo no uso pero seguramente usaré más adelante además de crear en java la interfaz gráfica del main menu que no uso pero puede que en un futuro sí

--- trunk/TuringApp/src/com/cinnamon/is/MainMenu.java
+++ trunk/TuringApp/src/com/cinnamon/is/MainMenu.java
@@ -1,12 +1,21 @@
 package com.cinnamon.is;
 
+import com.cinnamon.is.custom.appeareance.CustomButton;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Path.FillType;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.View.MeasureSpec;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainMenu extends Activity implements OnClickListener {
 
@@ -23,6 +32,42 @@ public class MainMenu extends Activity implements OnClickListener {
 		inicializar();
 	}
 
+	/** No toquies esto que ahora no lo necesitamos pero puede que en un futuro si
+	 * es es layout del main pero hecho en java o parte de él.
+	 */
+	/*private View getContentView() {
+		LinearLayout layoutMain = new LinearLayout(this);
+		layoutMain.measure(android.widget.LinearLayout.LayoutParams.FILL_PARENT,
+				android.widget.LinearLayout.LayoutParams.FILL_PARENT);
+		layoutMain.setOrientation(LinearLayout.VERTICAL);
+		layoutMain.setBackgroundResource(R.drawable.menu);
+		
+		LinearLayout layoutText = new LinearLayout(this);
+		layoutText.setGravity(Gravity.CENTER);
+		layoutText.measure(android.widget.LinearLayout.LayoutParams.FILL_PARENT,144);
+		layoutText.setOrientation(LinearLayout.VERTICAL);
+		
+		TextView textTitulo = new TextView(this);
+		textTitulo.setWidth(android.widget.LinearLayout.LayoutParams.FILL_PARENT);
+		textTitulo.setHeight(android.widget.LinearLayout.LayoutParams.WRAP_CONTENT);
+		textTitulo.setText("Hola");
+		textTitulo.setTextSize(100);
+		textTitulo.setTextColor(Color.WHITE);
+		
+		CustomButton bNuevaPartida = new CustomButton(this,R.drawable.b_nueva_partida,"bNuevaPartida");
+		bNuevaPartida = new CustomButton(this, R.drawable.b_nueva_partida, "bNuevaPartida");
+		bNuevaPartida.setOnClickListener(this);
+		bNuevaPartida.setGravity(Gravity.CENTER);
+		
+		
+		
+		//layoutText.addView(textTitulo);
+		layoutText.addView(bNuevaPartida);
+		layoutMain.addView(layoutText);
+		
+		return layoutMain;
+	}*/
+
 	private void inicializar() {
 		menuTheme = MediaPlayer.create(MainMenu.this, R.raw.menu);
 		menuTheme.start();
@@ -31,7 +76,7 @@ public class MainMenu extends Activity implements OnClickListener {
 		bOpciones = (Button) findViewById(R.id.bOpciones);
 		bAyuda = (Button) findViewById(R.id.bAyuda);
 		bSalir = (Button) findViewById(R.id.bSalir);		
-
+		
 		bNuevaPartida.setOnClickListener(this);
 		bOpciones.setOnClickListener(this);
 		bAyuda.setOnClickListener(this);
@@ -39,7 +84,7 @@ public class MainMenu extends Activity implements OnClickListener {
 	}
 
 	@Override
-	public void onClick(View v) {
+	public void onClick(View v) {		
 		switch (v.getId()) {
 		case R.id.bNuevaPartida:
 			menuTheme.release();
