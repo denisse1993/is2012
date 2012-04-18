9335194a071adf191c0deeb695f2cf902b679fd2
BuStAkAS@gmail.com <BuStAkAS@gmail.com@2ab73cf0-c2b9-5e7a-3472-acf655e2b59c>
2012-04-18 18:12:42 +0000

    ajustada tabla textviews y añadido total

--- trunk/Qronos/src/com/cinnamon/is/game/Ranking.java
+++ trunk/Qronos/src/com/cinnamon/is/game/Ranking.java
@@ -38,17 +38,18 @@ public class Ranking extends Activity {
 	 */
 	private void rankingLocal() {
 		int[] a = jugador.getScore();
+		String suma="";
 		setFila(1, jugador.getNombre(), String.valueOf(a[0]),
 				String.valueOf(a[1]), String.valueOf(a[2]),
 				String.valueOf(a[3]), String.valueOf(a[4]),
 				String.valueOf(a[5]), String.valueOf(a[6]),
 				String.valueOf(a[7]), String.valueOf(a[8]),
 				String.valueOf(a[9]), String.valueOf(a[10]),
-				String.valueOf(a[11]));
+				String.valueOf(a[11]), suma);
 	}
 
 	public boolean rankingOnline() {
-		String nick, mj1, mj2, mj3, mj4, mj5, mj6, mj7, mj8, mj9, mj10, mj11, mj12;
+		String nick, mj1, mj2, mj3, mj4, mj5, mj6, mj7, mj8, mj9, mj10, mj11, mj12, total;
 
 		JSONArray jArray;
 		try {
@@ -70,8 +71,9 @@ public class Ranking extends Activity {
 				mj10 = json_data.getString("MJ10");
 				mj11 = json_data.getString("MJ11");
 				mj12 = json_data.getString("MJ12");
+				total = json_data.getString("TOTAL");
 				setFila(j, nick, mj1, mj2, mj3, mj4, mj5, mj6, mj7, mj8, mj9,
-						mj10, mj11, mj12);
+						mj10, mj11, mj12,total);
 			}
 		} catch (JSONException e1) {
 			Toast.makeText(getBaseContext(), "Error", Toast.LENGTH_LONG).show();
@@ -83,7 +85,7 @@ public class Ranking extends Activity {
 
 	public void setFila(int i, String nick, String mj1, String mj2, String mj3,
 			String mj4, String mj5, String mj6, String mj7, String mj8,
-			String mj9, String mj10, String mj11, String mj12) {
+			String mj9, String mj10, String mj11, String mj12, String total) {
 		if (i == 1) {
 			TextView text0 = (TextView) findViewById(R.id.textView0);
 			text0.setText(nick);
@@ -111,6 +113,8 @@ public class Ranking extends Activity {
 			text11.setText(mj11);
 			TextView text12 = (TextView) findViewById(R.id.textView12);
 			text12.setText(mj12);
+			TextView text13 = (TextView) findViewById(R.id.textViewT1);
+			text13.setText(total);
 		}
 		if (i == 2) {
 			TextView text0 = (TextView) findViewById(R.id.textView13);
@@ -139,6 +143,8 @@ public class Ranking extends Activity {
 			text11.setText(mj11);
 			TextView text12 = (TextView) findViewById(R.id.textView25);
 			text12.setText(mj12);
+			TextView text13 = (TextView) findViewById(R.id.textViewT2);
+			text13.setText(total);
 		}
 		if (i == 3) {
 			TextView text0 = (TextView) findViewById(R.id.textView26);
@@ -167,6 +173,8 @@ public class Ranking extends Activity {
 			text11.setText(mj11);
 			TextView text12 = (TextView) findViewById(R.id.textView38);
 			text12.setText(mj12);
+			TextView text13 = (TextView) findViewById(R.id.textViewT3);
+			text13.setText(total);
 		}
 		if (i == 4) {
 			TextView text0 = (TextView) findViewById(R.id.textView39);
@@ -195,6 +203,9 @@ public class Ranking extends Activity {
 			text11.setText(mj11);
 			TextView text12 = (TextView) findViewById(R.id.textView51);
 			text12.setText(mj12);
+			
+			TextView text13 = (TextView) findViewById(R.id.textViewT4);
+			text13.setText(total);
 		}
 		if (i == 5) {
 			TextView text0 = (TextView) findViewById(R.id.textView52);
@@ -223,6 +234,8 @@ public class Ranking extends Activity {
 			text11.setText(mj11);
 			TextView text12 = (TextView) findViewById(R.id.textView64);
 			text12.setText(mj12);
+			TextView text13 = (TextView) findViewById(R.id.textViewT5);
+			text13.setText(total);
 		}
 
 		// handler=new Handler();
